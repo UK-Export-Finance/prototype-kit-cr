36fe89c1db431741ad63f3adb75ed600afe6c74d
ricky saunders <ricky.saunders@digital.hmrc.gov.uk>
2023-09-25 21:21:22 +0100

    Added date of defualta nd conversion to months

--- app/routes/routes-v1.js
+++ app/routes/routes-v1.js
@@ -62,6 +62,54 @@ router.post(version +'/1-pre-examination/notified-or-expected', function(req, re
 			res.redirect(version +'/facility-card-list')
 	}
 });
+router.post(version +'/1-pre-examination/date-of-default', function(req, res) {
 
+	{
+
+		if (req.session.data.defaultMonth == '1') {
+			req.session.data.defaultMonthAsText= 'January'
+		}
+		if (req.session.data.defaultMonth == '2') {
+			req.session.data.defaultMonthAsText= 'February'
+		}
+		if (req.session.data.defaultMonth == '3') {
+			req.session.data.defaultMonthAsText= 'March'
+		}
+		if (req.session.data.defaultMonth == '4') {
+			req.session.data.defaultMonthAsText= 'April'
+		}
+		if (req.session.data.defaultMonth == '5') {
+			req.session.data.defaultMonthAsText= 'May'
+		}
+		if (req.session.data.defaultMonth == '6') {
+			req.session.data.defaultMonthAsText= 'June'
+		}
+		if (req.session.data.defaultMonth == '7') {
+			req.session.data.defaultMonthAsText= 'July'
+		}
+		if (req.session.data.defaultMonth == '8') {
+			req.session.data.defaultMonthAsText= 'August'
+		}
+		if (req.session.data.defaultMonth == '9') {
+			req.session.data.defaultMonthAsText= 'September'
+		}
+		if (req.session.data.defaultMonth == '10') {
+			req.session.data.defaultMonthAsText= 'October'
+		}
+		if (req.session.data.defaultMonth == '11') {
+			req.session.data.defaultMonthAsText= 'November'
+		}
+		if (req.session.data.defaultMonth == '12') {
+			req.session.data.defaultMonthAsText= 'December'
+		}
+
+
+
+
+		req.session.data.defaultDate = req.session.data.defaultDay +" "+ req.session.data.defaultMonthAsText +" "+ req.session.data.defaultYear
+
+			res.redirect(version +'/facility-card-list')
+	}
+});
 
 }

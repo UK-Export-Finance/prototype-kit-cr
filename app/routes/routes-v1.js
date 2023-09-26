182d97c2f518fb34e321f3bdcc640b5d35f391e1
ricky saunders <ricky.saunders@digital.hmrc.gov.uk>
2023-09-26 11:10:26 +0100

    updated with sub sections

--- app/routes/routes-v1.js
+++ app/routes/routes-v1.js
@@ -6,20 +6,6 @@ router.post(version +'/setup', function(req, res) {
 	res.redirect(version +'/ukef-tiles')
 		});
 
-///////CREATE//////////
-
-	//use fresh posted data or use session data
-	//if (req.body['setupCategories'] == 'yes')
-
-	//SET UP PAGE//
-//	if (req.session.data.setupCategories == 'yes')
-//	{
-//		res.redirect(version +'/create/activity-type-select-with-category')
-//	}
-//	else {
-//		res.redirect(version +'/create/activity-name')
-//	}
-
 router.post(version +'/url', function(req, res) {
 	{
 		res.redirect(version +'/url')
